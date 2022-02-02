826434b5c7257b29b6c0c8ea927dfa8651226749
Roni Hell <prosto.lebed2014@yandex.ru>
2022-02-02 18:06:07 +0300

    main added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int ticket = 8000;
+        int percent = 20;
+
+        int miles = ticket / percent;
+        System.out.println(miles);
+    }
+}
